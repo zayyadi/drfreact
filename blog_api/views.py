70ba22029d1b869ed296b8ee6226abfafa65148f
Zayyad Musa <alias.co>
2021-10-13 10:32:26 +0100

    newest commit

---
+++ blog_api/views.py
@@ -0,0 +1,28 @@
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
+from rest_framework.permissions import SAFE_METHODS, BasePermission, IsAdminUser, DjangoModelPermissions, IsAuthenticatedOrReadOnly
+
+
+class PostUserWritePermission(BasePermission):
+    message = "Editing posts is restricted to the author only!"
+
+    def has_object_permission(self,request,view,obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.author == request.user
+
+class PostList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
+    pass
+
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
+    permission_classes = [PostUserWritePermission]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
